0d4fc9037a97e0955fe09b1e372f64cde91c1738
Aakash Sharma <aakashsharma9855@gmail.com>
2026-09-22 23:09:33 +0400

    feat: add calculation methods endpoint and documentation

--- content/api-reference/_meta.js
+++ content/api-reference/_meta.js
@@ -1,6 +1,7 @@
 export default {
   index: 'Overview',
   health: 'Health',
+  'calculation-methods': 'Calculation Methods',
   'prayer-times': 'Prayer Times',
   'prayer-times-range': 'Prayer Times Range'
 }
